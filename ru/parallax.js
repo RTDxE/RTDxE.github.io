be4b07236cdf99bb3964e91842119ef651dce264
Timofffee <timofffee@gmail.com>
2023-10-10 08:50:16 +0400

    Russian version of site

---
+++ ru/parallax.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const backgrounds = document.querySelectorAll(".background");
+
+//     window.addEventListener("scroll", function () {
+//         backgrounds.forEach((background) => {
+//             const scrollValue = window.scrollY / 2;
+//             background.style.transform = `translate3d(0, ${scrollValue}px, 0)`;
+//         });
+//     });
+// });
